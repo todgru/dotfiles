8e290fe480b40045f8b78e1a7dd549fcf2891e1d
todgru <todgru@gmail.com>
2019-12-17 11:05:16 -0800

    add private.zsh for work app env vars

--- source-me.zsh
+++ source-me.zsh
@@ -30,3 +30,6 @@ export PATH="/usr/local/opt/postgresql@9.6/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Application env vars
+[ -s "$HOME/dotfiles/private.zsh" ] && \. "$HOME/dotfiles/private.zsh"
